a13c2ec941bf6741e8107b84ed4f00b514771b7a
zongzhou.fang <zongzhou.fang@wormpex.com>
2018-04-02 12:19:41 +0800

    x

---
+++ src/main/java/Stream.java
@@ -0,0 +1,6 @@
+public class Stream {
+    public static void main(String[] args) {
+        String string="fa   zongz";
+        System.out.println(string.replace(" ", ""));
+    }
+}
---
+++ src/main/java/TestBean.java
@@ -0,0 +1,9 @@
+
+public class TestBean {
+    public static void main(String[] args) {
+        String a = "asdf";
+        String b = "asdf";
+        String c=b;
+        System.out.println(a.equals(c));
+    }
+}
